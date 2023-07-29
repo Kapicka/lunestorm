6fbb31670110a85228cb83745e6ad786c8e0d4e8
Ondrej Hanus <hanus.oh@gmail.com>
2023-07-29 23:25:32 +0200

    feat: add new event

--- src/store.js
+++ src/store.js
@@ -2,20 +2,20 @@ const theme = { value: "electro" };
 
 const events = [
   {
-    key: "electroecumene",
+    key: "electroecumene_11_2023",
     type: ["electro", "ecumene"],
     name: "Oecumena Electronica",
-    dateStart: "2022-11-18T18:30",
-    dateEnd: "2022-11-18T21:00",
+    dateStart: "2023-11-03T18:30",
+    dateEnd: "2023-11-03T20:00",
     links: [
-      { name: "fb", url: "https://www.facebook.com/events/1131298834180662" },
-      { name: "ig", url: "https://www.instagram.com/oecumena_electronica/" }
+      // { name: "fb", url: "https://www.facebook.com/events/1131298834180662" },
+      // { name: "ig", url: "https://www.instagram.com/oecumena_electronica/" }
     ],
     place: {
       name: "Kostel sv. Martina ve zdi",
       url: "https://www.google.com/maps/place/The+Church+of+St.+Martin+in+the+Wall/@50.0831299,14.4198462,15z/data=!4m5!3m4!1s0x0:0x7a4479b958a5120e!8m2!3d50.0831016!4d14.419851"
     },
-    artists: ["grafik"]
+    artists: []
     // description: "Zveme vás na elektro-ekumenu!"
   },
   {
@@ -39,7 +39,7 @@ const events = [
       url: "https://www.facebook.com/KlubCajovna"
     },
     links: [
-      { name: "fb", url: "https://fb.me/e/2AM11QFN2" },
+      { name: "fb", url: "https://fb.me/e/2AM11QFN2" }
     ],
     dateStart: "2023-06-17T13:00",
     dateEnd: "2023-06-18T04:00"
@@ -48,13 +48,13 @@ const events = [
     key: "lunestorm_live",
     type: ["electro"],
     name: "LUNESTORM LIVE",
-    links: [ { name: "fb", url: "https://fb.me/e/2DSyQ2Xia" },
+    links: [{ name: "fb", url: "https://fb.me/e/2DSyQ2Xia" }
     ],
     place: {
       name: "Vacuum Art Space",
       url: "https://www.google.com/maps/place/Vacuum+Art+Space,+z.s./@50.0662146,14.4491341,17z/data=!3m1!4b1!4m6!3m5!1s0x470b938025f534dd:0x545a2d97e45572da!8m2!3d50.0662112!4d14.4513228!16s%2Fg%2F11cpkylc8g"
     },
-    artists: ["grafik","infract","bena"],
+    artists: ["grafik", "infract", "bena"],
     dateStart: "2023-02-11T20:00",
     dateEnd: "2023-02-12T08:00"
   }
@@ -74,13 +74,13 @@ const artists = [
     name: "Infract",
     type: ["electro", "ecumene"],
     key: "infract",
-    links: [ ]
+    links: []
   },
   {
     name: "Beña",
     type: ["electro", "ecumene"],
     key: "bena",
-    links: [ ]
+    links: []
   }
 ];
 
